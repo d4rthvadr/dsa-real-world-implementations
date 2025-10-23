f030ee512519d9edc5ca4d88e7a68f1ddc6d279b
darkvadr <glowburn.acc@gmail.com>
2025-10-23 21:25:21 +0000

    add get adjacent nodes func

---
+++ python/graphs-03.py
@@ -0,0 +1,43 @@
+class Graph:
+
+    
+    def adjacent_nodes(self, node):
+        
+        if not node in self.graph:
+            return None 
+        
+        return self.graph[node]
+
+            
+            
+
+    # don't touch below this line
+
+    def __init__(self):
+        self.graph = {}
+
+    def add_edge(self, u, v):
+        self.graph.setdefault(u, set()).add(v)   
+        self.graph.setdefault(v, set()).add(u)   
+
+
+
+def main():
+    
+    g = Graph()
+    graph = Graph()
+    graph.add_edge(0, 1)
+    graph.add_edge(0, 2)
+    graph.add_edge(1, 3)
+    graph.add_edge(2, 3)
+
+    adjacent_nodes = graph.adjacent_nodes(1)
+    # {0, 3}
+    a = set()
+    a.add(2)
+    a.add(4)
+    a.add(10)
+    print("a-->",a)
+    print("print graph", adjacent_nodes)
+    
+main()    
